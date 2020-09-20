d17ecef12367bf6bbd1097d85ecbd0f891946dc6
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-09-19 17:03:34 -0700

    Add Assignment 161

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //And
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Or
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            //Equal
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            //Not Equal
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            //XOR True if one is true, but not both
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.WriteLine(true && true && true || false);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ BooleanLogic_Assignment/BooleanLogic_Assignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Application");
+
+            Console.WriteLine("What is your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine("Have you ever had a DUI? Write \"true\" if you have had a DUI or \"false\" if you haven't.");
+            bool dui = bool.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool qualified = age > 15 && dui == false && tickets <= 3;
+            Console.Write(qualified);
+            Console.ReadLine();
+
+
+
+
+
+
+
+            //Console.WriteLine(" Annual salary of Person 1 ");
+            //decimal annualSum = total * 52;
+            //Console.WriteLine(annualSum);
+            //Console.ReadLine();
+
+            //Console.WriteLine(" Annual salary of Person 2 ");
+            //decimal annualSum2 = total2 * 52;
+            //Console.WriteLine(annualSum2);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Does Person 1 make more money than Person 2?");
+            //bool trueOrFalse = annualSum > annualSum2;
+            //Console.Write(trueOrFalse.ToString());
+            //Console.ReadLine();
+        }
+    }
+}
