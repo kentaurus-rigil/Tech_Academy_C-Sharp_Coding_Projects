4caaa2f14703bb690d7073b6e0da12a2c54af779
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-09-21 17:55:54 -0700

    Add Assignment 186

---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jesse";
+            name = "Joe";  //doesn't change the string it creates a new string.  Too many can slow down the system.
+            //Can use string builders to dynamically change the string
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Jesse");
+
+            //Functions With Strings:
+
+            //string name = "Star";
+            //string quote = "The man said, \"Hello\", Star. \n \\Hello on a new line. \n \t Hello on a tab.";
+            //string fileName = "C:\\Users\\Jesse";
+            //string fileName2 = @"C:\Users\Jesee";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+            //Console.WriteLine(trueOrFalse);
+
+            //int length = name.Length;
+            //Console.WriteLine(length);
+
+            //name = name.ToUpper(); //Can also ToLower();
+            //Console.WriteLine(name);
+
+
+
+            //Console.WriteLine(quote);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ stringAssignment/stringAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Concatenate three strings.
+            string userName = "<Type your name here>";
+            string date = DateTime.Today.ToShortDateString();
+
+            string str = $"Hello {userName}.  Today is {date}.";
+            System.Console.WriteLine(str);
+            Console.ReadLine();
+
+
+            //Converts a string to uppercase.
+            string name = "Star";
+            name = name.ToUpper(); //Can also ToLower();
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+
+            //Stringbuilder creates a paragrph of text, one sentence at a time.
+            string names = "Gardner";
+            names = "Gardner";  //doesn't change the string it creates a new string.  Too many can slow down the system.
+            //Can use string builders to dynamically change the string
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Gardner" + "\n We gave you a chance" + "\n To water the plants." + "\n We didn't mean that way-" + "\n Now zip up your pants." + "\n By Shel Silverstein");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
