88af2414e0d23e36e85e196e9dea516107308e85
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-09-21 20:13:31 -0700

    Add Assignment 191

---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //List very common and most used feature
+        List<int> intList = new List<int>();
+        intList.Add(4);
+        intList.Add(10);
+        intList.Remove(10);
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+
+
+        //String List
+        List<string> intList2 = new List<string>();
+        intList2.Add("Hello");
+        intList2.Add("Star");
+        intList2.Remove("Star");
+        Console.WriteLine(intList2[0]);
+        Console.ReadLine();
+
+
+        //Use an array for a fixed quantity, because difficult to change
+        //Intialize an array
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+        //Console.WriteLine(numArray[3]);
+
+        ////Intialized the same array, but more efficiently
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+        //Console.WriteLine(numArray1[3]);
+
+        ////Instance of an array the most efficient way
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        ////To change an item in the array identify its index number and write the new item.
+        //numArray2[5] = 650;
+        //Console.WriteLine(numArray2[5]);
+
+
+
+        //Use for a very large quantity (e.g. byteArray)
+        //byte[] byteArray = new int[5];
+
+
+
+
+
+
+        Console.ReadLine();
+
+        }
+    }
+
---
+++ ListAndArrayAssignment/ListAndArrayAssignment/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //Part 1 Array of strings and part 3 display when user selects an index that doesn't exist.
+        
+        string[] stringArray = new string[6];
+        stringArray[0] = "As I see it, yes.";
+        stringArray[1] = "Don't count on it.";
+        stringArray[2] = "It is decidedly so.";
+        stringArray[3] = "It is certain.";
+        stringArray[4] = "Outlook not so good.";
+        stringArray[5] = "My sources say no.";
+        Console.WriteLine("Think of a \'yes\' or \'no\' question. Then type a number between 0-5 to receive your answer.");
+        int index = Convert.ToInt32(Console.ReadLine());
+        if(index > 5 || index < 0)
+        {
+            Console.WriteLine("Your index selection is not between 0-5.");
+        }
+        else
+        {
+            Console.WriteLine("Answer:" + stringArray[index]);
+            
+        }
+        Console.ReadLine();
+
+
+        //Part 2 Array of integers and part 3 display when user selects an index that doesn't exist.
+        int[] numArray = { 23, 10, 105, 700, 5050, 900, 23 };
+        Console.WriteLine("Type a number between 0-6 to see data for that index.");
+        int index1 = Convert.ToInt32(Console.ReadLine());
+        if (index1 > 6 || index1 < 0)
+        {
+            Console.WriteLine("Your index selection is not between 0-5.");
+        }
+        else
+        {
+            Console.WriteLine(numArray[index1]);
+
+        }
+        Console.ReadLine();
+
+
+
+        //Part 4
+        List<string> intList = new List<string>();
+        intList.Add("Hawaii");
+        intList.Add("Alaska");
+        intList.Add("California");
+        intList.Add("New York");
+        intList.Add("Oregon");
+        intList.Add("Florida");
+        Console.WriteLine("Type a number between 0-5 to see a state in the index.");
+        int index2 = Convert.ToInt32(Console.ReadLine());
+        if (index2 > 5 || index2 < 0)
+        {
+            Console.WriteLine("Your index selection is not between 0-5.");
+        }
+        else
+        {
+            Console.WriteLine("The State:" + intList[index2]);
+
+        }
+
+        Console.ReadLine();
+
+    }
+}
+          
