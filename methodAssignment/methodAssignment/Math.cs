e5d88972a268c5ebce2bd25d860e74a82983ed01
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-12 17:28:06 -0700

    Add Assignment 216

---
+++ methodAssignment/methodAssignment/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssignment
+{
+    public class Math
+    {
+        public int methodOne(int x)
+        {
+            int results = x + 5;
+            return results;
+        }
+
+        public int methodTwo(int x)    
+        {
+            int results = x - 10;
+            return results;
+        }
+        public int methodThree(int x)
+        {
+            int results = x / 2;
+            return results;
+        }
+    }
+}
---
+++ methodAssignment/methodAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodAssignment
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Math ObjOne = new Math();
+            Console.WriteLine("Enter a number.");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine (ObjOne.methodOne(numOne));
+            Console.WriteLine(ObjOne.methodTwo(numOne));
+            Console.WriteLine(ObjOne.methodThree(numOne));
+            Console.ReadLine();
+        }
+        
+       
+    }
+}
