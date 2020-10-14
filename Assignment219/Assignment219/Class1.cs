b2eeecb577d7f9cff1df8cd0eab77e115cc4365e
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-13 17:31:27 -0700

    Add Assignment 219

---
+++ Assignment219/Assignment219/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment219
+{
+    public class Class1
+    {
+        public int method(int x)
+        {
+            int results = x + 5;
+            return results;
+        }
+        public double method(double x, double y)
+        {
+            return x - y;
+        }
+        public int method(string x, string z)
+        {
+            
+            int newNum = Convert.ToInt32(x);
+            int newNum2 = Convert.ToInt32(z);
+            return newNum / newNum2;
+
+        }
+    }
+    }
+
+
+
+
+//public int methodThree(int x)
+//{
+//    int results = x / 2;
+//    return results;
+//}
+//    }
+//}
---
+++ Assignment219/Assignment219/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment219
+{
+    class Program
+    {
+            
+            static void Main(string[] args)
+            {
+            Class1 ObjOne = new Class1();
+                int myNum1 = ObjOne.method(100);
+                double myNum2 = ObjOne.method(4.3, 6.26);
+                int myNum3 = ObjOne.method("200", "100");
+                Console.WriteLine("Int: " + myNum1);
+                Console.WriteLine("Double: " + myNum2);
+                Console.WriteLine("String: " + myNum3);
+                Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+//Console.WriteLine("Enter a number.");
+//            int numOne = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(ObjOne.methodOne(numOne));
+//            Console.ReadLine();
+//        }
+//Class1 ObjOne = new Class1();
+//int numTwo = Convert.ToInt32(Console.ReadLine());
+//Console.Writeline(ObjOne.methodOne(numTwo));
