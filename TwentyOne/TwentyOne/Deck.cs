efab502662c69b6c551ee5ebb06ec27ccf0b40b3
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-14 18:12:14 -0700

    Add Card Game 21

---
+++ TwentyOne/TwentyOne/Deck.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
+            List<string> Faces = new List<string>()
+            {
+                "Two", "Three","Four", "Five", "Six", "Seven",
+                "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+            };
+
+            foreach (string face in Faces)
+            {
+                foreach (string suit in Suits)
+                {
+                    Card card = new Card();
+                    card.Suits = suit;
+                    card.Faces = face;
+                    Cards.Add(card);
+                }
+            }
+            //With this solution we would have to create 52 cards and not efficient.
+            //Cards = new List<Card>();
+            //Card cardOne = new Card();
+            //cardOne.Face = "Two";
+            //Cards.Add(cardOne);
+        }
+        public List<Card> Cards { get; set; }
+        public  void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count);
+                    TempList.Add(Cards[randomIndex]);
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+
+            }
+            
+        }
+    }
+}
+
+
---
+++ TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+            //deck = Shuffle(deck);
+            //deck = Shuffle(/*named parameter easier to read*/deck: deck, times: 3);//optional parameters
+            
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Faces + " of " + card.Suits);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+
+        //Method Overloading
+        //public static Deck Shuffle(Deck deck, int times) 
+        //{
+        //    for (int i = 0; i < times; i++)
+        //    {
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
+            //deck.Cards = new List<Card>();
+
+
+            //Card cardOne = new Card();
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
+
+            //deck.Cards.Add(cardOne);
+
+            //Console.WriteLine(cardOne.Face + " of " + cardOne.Suit);
+            //Console.ReadLine();
+
+        }
+    }
+
+
