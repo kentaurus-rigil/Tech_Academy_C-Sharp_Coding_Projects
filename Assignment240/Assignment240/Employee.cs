2149a5e92b9054e40dafa4a0b29afbe551884d60
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-16 14:32:08 -0700

    Add Assignment 240

---
+++ Assignment240/Assignment240/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment240
+{
+    public class Employee : Person, IQuittable
+    {
+        public void Quit() 
+        {
+            Console.WriteLine("This employee quit.");
+        }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name:" + firstName + " " + lastName);
+        }
+        
+
+        
+        
+
+        
+
+
+    }
+
+
+
+}
+
+
