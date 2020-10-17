3ffba1cdbf1790b89fc85cf0d1c125b3f2d26447
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-16 21:04:56 -0700

    Add enumAssignment

---
+++ enumAssignment/enumAssignment/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tryIt = true;
+            while (tryIt)
+            {
+                try
+                {
+
+
+                    Console.WriteLine("Please enter the current day of the week.");
+                    string weekDay = Console.ReadLine();
+                    DaysOfTheWeek userDay = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), weekDay, true);
+                    Console.WriteLine("The day of the week is" + userDay + ".");
+                    Console.ReadLine();
+                    tryIt = false;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message); //or ("")
+                                                   //return; (stops the operation.
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Please don't divide by zero."); //(ex.Message)
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+
+           public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    
+        
+}
+}
+        
