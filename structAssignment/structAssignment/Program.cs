1eeb5fb69ca3a1da86976ef603d62c9e8eb8c7c4
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-18 08:15:49 -0700

    Add Assignment 260

---
+++ structAssignment/structAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number amount1 = new Number();
+            amount1.Amount = 389.5m;
+            Console.WriteLine(amount1.Amount);
+            Console.ReadLine();
+
+
+        }
+    }
+}
