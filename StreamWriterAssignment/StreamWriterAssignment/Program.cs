23939e8aff4332961cf77808ae2d93261f0dd3e9
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-20 19:28:50 -0700

    Add Assignment 290

---
+++ StreamWriterAssignment/StreamWriterAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamWriterAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string userNum = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\guesttta\Documents\GitHub\Tech_Academy_C-Sharp_Coding_Projects\SWAssignment.txt", true))
+            {
+                file.WriteLine(userNum);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
