f225bb603cf98ec121b747f4e2eccb476a540b73
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-21 10:42:57 -0700

    Add Assignment 293

---
+++ dateAssignment/dateAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace dateAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Current Date and Time is: ");
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime);
+            Console.WriteLine("Please enter a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            DateTime newTime = currentTime.AddHours(userNum);
+            Console.WriteLine(newTime);
+            Console.ReadLine();
+                
+        }
+    }
+}
