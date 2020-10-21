39dd89bb71e9e45ac1d64320b52b54ea2741ce83
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-20 19:29:35 -0700

    Update Game 21-streamWriter

--- 21/21/Program.cs
+++ 21/21/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _21
 {
@@ -10,6 +11,8 @@ namespace _21
     {
         static void Main(string[] args)
         {
+          
+
             Console.WriteLine("Welcome to the Grand Hotel and Casino.  Let's start by telling me your name.");
             string playerName = Console.ReadLine();
             Console.WriteLine("And how much money did you bring today?");
