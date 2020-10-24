4fedaaaf1a8e92787a293a7411bdbebc18bd35f5
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-24 14:24:22 -0700

    Update Game 21

--- 21/21/Program.cs
+++ 21/21/Program.cs
@@ -12,11 +12,21 @@ namespace TwentyOne
             const string casinoName = "Grand Hotel and Casino";
 
             //Guid identifier = Guid.NewGuid();
-
             Console.WriteLine("Welcome to the {0}.  Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+                while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
+            
+          
+
+            
             Console.WriteLine("Hello, {0}.  Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
             if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya") 
@@ -32,7 +42,22 @@ namespace TwentyOne
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try 
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security kick this person out.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("An error occurred.  Please contact your System Administrator.");
+                        Console.ReadLine();
+                        return;
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
