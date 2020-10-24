9ae668c38147dde84acb5f2f628d7794a9209e43
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-24 16:22:14 -0700

    Update assignement 303

--- ConstantAssignment2/ConstantAssignment2/Employee.cs
+++ ConstantAssignment2/ConstantAssignment2/Employee.cs
@@ -17,8 +17,7 @@ namespace ConstantAssignment2
         }
         public string Name { get; set; }
         public int EmployeeID { get; set; }
-        Employee emp = new Employee("This is a name");
-        Console.WriteLine(emp.EmployeeID);
+        
     }
 }
     
