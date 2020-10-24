d8bf3b4a785172b4d5851cbef976feeb4ad4dd93
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-24 16:21:45 -0700

    Add assignment 307

---
+++ ExceptionAssignment2/ExceptionAssignment2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int age = 0;
+            try
+            {
+                Console.WriteLine("Please type your age.");
+                age = Convert.ToInt32(Console.ReadLine());
+
+                if (age == 0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    DateTime current = DateTime.Now;
+                    int birthYear = current.Year - age;
+                    Console.WriteLine("Your birth year is: " + birthYear);
+                }
+                Console.ReadLine();
+
+            }
+
+            catch (ArgumentException)
+            {
+                Console.WriteLine("You may not enter zero for your age.");
+                Console.ReadLine();   
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+            
+            
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
