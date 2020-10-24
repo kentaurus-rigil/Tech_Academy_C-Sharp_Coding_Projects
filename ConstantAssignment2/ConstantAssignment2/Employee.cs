1de410e920b6ac8616a9abea9de67818875f83ba
kentaurus-rigil <66184504+kentaurus-rigil@users.noreply.github.com>
2020-10-24 13:33:38 -0700

    Add Assignment 303

---
+++ ConstantAssignment2/ConstantAssignment2/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstantAssignment2
+{
+    class Employee
+    {
+        public Employee(string name) : this(name, 2312)
+        {
+
+        }
+        public Employee(string name, int idNum)
+        {
+            Name = name;
+            EmployeeID = idNum;
+        }
+        public string Name { get; set; }
+        public int EmployeeID { get; set; }
+    }
+}
+    
+
---
+++ ConstantAssignment2/ConstantAssignment2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string employeeName = "Athletes";
+            Console.WriteLine(employeeName);
+
+            var newDictionary = new Dictionary<string, string>();
+        }
+    }
+}
